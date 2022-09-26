115754187cb5a47d069147bd1c850aea47611c37
Admin <Admin@FOREVEREST>
2022-09-26 19:11:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿float sum = 1;
+float num = int.Parse(Console.ReadLine());
+float x = num;
+float i = 1;
+float fact = 1;
+float degree = 1;
+float e = flo
+float degreeE = 1;
+while (sum <= Math.Pow(Math.E, num))
+{
+    Console.WriteLine(fact * i);
+    degree = degree * x;
+    fact = fact * i;
+    sum += (degree / fact);
+    i++;
+}
+
+Console.WriteLine(sum);
+
+//int i = 1;
+//int x = int.Parse(Console.ReadLine());
+//int fact = 1;
+//while (fact < 64)
+//{
+//    fact = fact * x;
+//}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//double eps = 0.001;
+//double num = double.Parse(Console.ReadLine());
+//double a = 0;
+
+//int i = 0; 
+
+//while (num >= eps)
+//{
+//    num = Math.Sin(num);
+//    Console.WriteLine(num);
+//    i++;
+//}
+
+//Console.WriteLine(num);
+//Console.WriteLine(i);
+
+
+ x = double.Parse(Console.ReadLine());
+int y = double.Parse(Console.ReadLine());
